fa5138574a9a587553b893045cc1959a874068f9
Kevin <mr.kev@me.com>
2014-05-24 03:00:46 -0500

    Initial commit to github. Set up and get working.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 StudentCenterThingy
 ===================
+
+A bot for Cornell's student center. Idk. Maybe it can be eventually developed to automatically check when a spot opens in a class or something?
+
+I'm not sure if this is allowed. Use at your own risk.
+
+##Usage
+
+1. In nodebot.js, replace `NETID` with your NetID and `PASSWORD` with your password. 
+
+2. Run it using `node nodebot.js`
+
+3. A screenshot of StudentCenter should appear as screen.png
+
+I know, it's unsafe and really bare bones. It's just a little experiment I put together out of curiosity one day in the library after reading a post in the CS Facebook Group. Feel free to expand on it.
---
+++ nodebot.js
@@ -0,0 +1,48 @@
+var phantom=require('node-phantom');
+phantom.create(start, {});
+
+
+
+function start (err, phantom) {
+ // var page = require('webpage').create();
+
+  return phantom.createPage(function(err, page) {
+
+    console.log('Tick');
+
+    page.onConsoleMessage = function(msg) {
+        console.log(msg);
+    };
+
+
+    page.open("http://studentcenter.cornell.edu", function(err, status) {
+      console.log('Tick' + status);
+
+        if ( status === "success" ) {
+          console.log('Tick');
+
+            page.evaluate(function() {
+                  // console.log(document.documentElement.innerHTML);
+
+                  document.querySelector("input[name='netid']").value    = "NETID";
+                  document.querySelector("input[name='password']").value = "PASSWORD";
+                  document.querySelector("form[name='login']").submit();
+
+                  console.log("Login submitted!");
+
+                  page.render('github.png');
+            });
+            console.log('Tick');
+
+
+            setTimeout(function () {
+              console.log('Tick');
+
+              page.render('screen.png');
+              phantom.exit();
+            }, 5000);
+       }
+    });
+  });
+  
+}
